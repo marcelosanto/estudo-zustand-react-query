69f5c7d7202108c358e9c3e7bc8a779619d0bfee
Marcelo Santos <rocha.mer21@gmail.com>
2022-01-09 16:05:41 -0300

    fazendo os testes

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import App from '../App'
+import Provider from '../Provider'
+import nock from 'nock'
+import { topMangas } from '../mocks/topMangas'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+nock('https://api.jikan.moe/v3/')
+  .persist()
+  .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+  .get('/top/manga')
+  .reply(200, topMangas)
+
+test('renders mangas sucessfully', async () => {
+  render(
+    <Provider>
+      <App />
+    </Provider>
+  )
+
+  const firstTopManga = topMangas.top[0].title
+
+  await waitFor(async () => {
+    const firtsMangaElement = screen.getByText(firstTopManga)
+    expect(firtsMangaElement).toBeInTheDocument()
+  })
+})
+
+test('match snapshot', async () => {
+  const { container } = render(
+    <Provider>
+      <App />
+    </Provider>
+  )
+
+  await expect(container).toMatchSnapshot()
 })
