57325972706ddb9bb045e1c0a8826ab7e526fee7
Marcelo Santos <rocha.mer21@gmail.com>
2022-01-09 12:05:08 -0300

    configurado o provider, react-query and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import useFetchTopMangas from './services/requests/useFetchTopMangas'
 
 function App() {
+  const { data } = useFetchTopMangas()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {data?.top.map((manga) => (
+        <div className='Test' key={`Manga-${manga.mal_id}`}>
+          <img src={manga.image_url} />
+          {manga.title}
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Provider.tsx
@@ -0,0 +1,13 @@
+import { QueryClientProvider } from 'react-query'
+
+import queryClient from './services/queryClient'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export default function Provider({ children }: Props) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
